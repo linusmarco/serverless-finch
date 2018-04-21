791baba1727a863fa2f2b1a42457086341587444
Linus Marco <lmarco163@gmail.com>
2018-04-21 17:19:04 -0400

    wrapping up tests for index.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -12,6 +12,9 @@ const hlp = require('./resources/helpers');
 const bucketUtils = require('../lib/bucketUtils');
 jest.mock('../lib/bucketUtils');
 
+const configure = require('../lib/configure');
+jest.mock('../lib/configure');
+
 const uploadDirectory = require('../lib/upload');
 jest.mock('../lib/upload');
 
@@ -107,14 +110,14 @@ describe('Client', () => {
     });
 
     it('should reject Bluebird promise if options invalid', () => {
-      validateClient.mockImplementation(() => 'fake error message');
-      pluginInstance.error = jest.fn();
+      validateClient.mockImplementation(() => {
+        throw ['fake error message'];
+      });
 
       pluginInstance._validateConfig();
 
       expect(BbPromise.reject).toHaveBeenCalledTimes(1);
-      expect(pluginInstance.error).toHaveBeenCalledTimes(1);
-      expect(pluginInstance.error).toHaveBeenCalledWith('fake error message');
+      expect(BbPromise.reject).toHaveBeenCalledWith(expect.stringContaining('fake error message'));
     });
 
     it('should do nothing if options valid', () => {
@@ -135,44 +138,273 @@ describe('Client', () => {
       serverless = new Serverless();
       pluginInstance = new FinchClient(serverless, undefined);
 
-      // serverless doesn't actually set options til hooks are called so need to set them here
-      const simpleConfig = hlp.readTestConfig('valid/simple');
-      pluginInstance.options = simpleConfig;
-      pluginInstance._validateConfig = jest.fn();
+      pluginInstance._validateConfig = jest.fn(() => Promise.resolve());
+      pluginInstance.options = { bucketName: 'my-test-bucket' };
+      pluginInstance.serverless = { cli: { log: jest.fn() } };
       pluginInstance.error = jest.fn();
 
       Confirm.mockImplementation(function() {
         this.run = jest.fn(() => Promise.resolve(true));
       });
+
+      bucketUtils.bucketExists.mockImplementation(() => Promise.resolve(true));
+      bucketUtils.emptyBucket.mockImplementation(() => Promise.resolve());
+      bucketUtils.deleteBucket.mockImplementation(() => Promise.resolve());
     });
 
     it('should validate config', () => {
-      pluginInstance._removeDeployedResources();
-
-      expect(pluginInstance._validateConfig).toHaveBeenCalledTimes(1);
+      return pluginInstance._removeDeployedResources().then(() => {
+        expect(pluginInstance._validateConfig).toHaveBeenCalledTimes(1);
+      });
     });
 
     it('should throw error if config invalid', () => {
-      const simpleConfig = hlp.readTestConfig('invalid/badBucketName');
-      pluginInstance.options = simpleConfig;
+      pluginInstance._validateConfig = jest.fn(() => Promise.reject('Some error message'));
 
-      expect(pluginInstance.error).toHaveBeenCalledTimes(1);
+      return pluginInstance._removeDeployedResources().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(1);
+        expect(pluginInstance.error).toHaveBeenCalledWith('Some error message');
+
+        expect(Confirm).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.deleteBucket).toHaveBeenCalledTimes(0);
+      });
     });
 
     it('should prompt user to confirm removal', () => {
-      pluginInstance._removeDeployedResources();
-
-      expect(Confirm).toHaveBeenCalledWith(
-        `Are you sure you want to delete bucket '${pluginInstance.options.bucketName}'?`
-      );
-      expect(Confirm.mock.instances.length).toBe(1);
-      expect(Confirm.mock.instances[0].run).toHaveBeenCalledTimes(1);
+      return pluginInstance._removeDeployedResources().then(() => {
+        expect(Confirm).toHaveBeenCalledWith(
+          `Are you sure you want to delete bucket '${pluginInstance.options.bucketName}'?`
+        );
+        expect(Confirm.mock.instances.length).toBe(1);
+        expect(Confirm.mock.instances[0].run).toHaveBeenCalledTimes(1);
+      });
     });
 
-    // it('should wait 3 seconds before deleting', () => {});
+    it('should quit if user does not confirm', () => {
+      Confirm.mockImplementation(function() {
+        this.run = jest.fn(() => Promise.resolve(false));
+      });
 
-    // it('should', () => {
-    //   hlp.readTestConfig('valid/simple');
-    // });
+      return pluginInstance._removeDeployedResources().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.deleteBucket).toHaveBeenCalledTimes(0);
+      });
+    });
+
+    it('should perform removal if user confirms', () => {
+      return pluginInstance._removeDeployedResources().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.deleteBucket).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    it('should not do anything if bucket does not exist', () => {
+      bucketUtils.bucketExists.mockImplementation(() => Promise.resolve(false));
+
+      return pluginInstance._removeDeployedResources().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.deleteBucket).toHaveBeenCalledTimes(0);
+      });
+    });
+  });
+
+  describe('#_processDeployment', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+
+      const validOptions = hlp.readTestConfig('valid/simple');
+
+      serverless = new Serverless();
+      pluginInstance = new FinchClient(serverless, undefined);
+
+      pluginInstance._validateConfig = jest.fn(() => Promise.resolve());
+      pluginInstance.options = validOptions.custom.client;
+      pluginInstance.serverless = {
+        cli: {
+          log: jest.fn()
+        },
+        config: {
+          servicePath: '/path/to/my/site'
+        }
+      };
+      pluginInstance.error = jest.fn();
+
+      Confirm.mockImplementation(function() {
+        this.run = jest.fn(() => Promise.resolve(true));
+      });
+
+      bucketUtils.bucketExists.mockImplementation(() => Promise.resolve(true));
+      bucketUtils.emptyBucket.mockImplementation(() => Promise.resolve());
+      bucketUtils.createBucket.mockImplementation(() => Promise.resolve());
+      configure.configureBucket.mockImplementation(() => Promise.resolve());
+      configure.configurePolicyForBucket.mockImplementation(() => Promise.resolve());
+      configure.configureCorsForBucket.mockImplementation(() => Promise.resolve());
+      uploadDirectory.mockImplementation(() => Promise.resolve());
+    });
+
+    it('should validate config', () => {
+      return pluginInstance._processDeployment().then(() => {
+        expect(pluginInstance._validateConfig).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    it('should throw error if config invalid', () => {
+      pluginInstance._validateConfig = jest.fn(() => Promise.reject('Some error message'));
+
+      return pluginInstance._processDeployment().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(1);
+        expect(pluginInstance.error).toHaveBeenCalledWith('Some error message');
+
+        expect(Confirm).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.createBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configurePolicyForBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureCorsForBucket).toHaveBeenCalledTimes(0);
+        expect(uploadDirectory).toHaveBeenCalledTimes(0);
+      });
+    });
+
+    it('should prompt user to confirm deploy', () => {
+      return pluginInstance._processDeployment().then(() => {
+        expect(Confirm).toHaveBeenCalledWith('Do you want to proceed?');
+        expect(Confirm.mock.instances.length).toBe(1);
+        expect(Confirm.mock.instances[0].run).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    it('should quit if user does not confirm', () => {
+      Confirm.mockImplementation(function() {
+        this.run = jest.fn(() => Promise.resolve(false));
+      });
+
+      return pluginInstance._processDeployment().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.createBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configurePolicyForBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureCorsForBucket).toHaveBeenCalledTimes(0);
+        expect(uploadDirectory).toHaveBeenCalledTimes(0);
+      });
+    });
+
+    it('should perform deployment if user confirms', () => {
+      return pluginInstance._processDeployment().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.createBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configurePolicyForBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configureCorsForBucket).toHaveBeenCalledTimes(1);
+        expect(uploadDirectory).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    it('should create bucket (and not empty) if bucket does not exist', () => {
+      bucketUtils.bucketExists.mockImplementation(() => Promise.resolve(false));
+
+      return pluginInstance._processDeployment().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.createBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configureBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configurePolicyForBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configureCorsForBucket).toHaveBeenCalledTimes(1);
+        expect(uploadDirectory).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    it('should not empty bucket if --no-delete-contents specified', () => {
+      pluginInstance.cliOptions['delete-contents'] = false;
+
+      return pluginInstance._processDeployment().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(0);
+        expect(bucketUtils.createBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configurePolicyForBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configureCorsForBucket).toHaveBeenCalledTimes(1);
+        expect(uploadDirectory).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    it('should not configure bucket if --no-config-change specified', () => {
+      pluginInstance.cliOptions['config-change'] = false;
+
+      return pluginInstance._processDeployment().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.createBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configurePolicyForBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configureCorsForBucket).toHaveBeenCalledTimes(1);
+        expect(uploadDirectory).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    it('should not set bucket policy if --no-policy-change specified', () => {
+      pluginInstance.cliOptions['policy-change'] = false;
+
+      return pluginInstance._processDeployment().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.createBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configurePolicyForBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureCorsForBucket).toHaveBeenCalledTimes(1);
+        expect(uploadDirectory).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    it('should not set cors policy if --no-cors-change specified', () => {
+      pluginInstance.cliOptions['cors-change'] = false;
+
+      return pluginInstance._processDeployment().then(() => {
+        expect(pluginInstance.error).toHaveBeenCalledTimes(0);
+
+        expect(Confirm).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.bucketExists).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.emptyBucket).toHaveBeenCalledTimes(1);
+        expect(bucketUtils.createBucket).toHaveBeenCalledTimes(0);
+        expect(configure.configureBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configurePolicyForBucket).toHaveBeenCalledTimes(1);
+        expect(configure.configureCorsForBucket).toHaveBeenCalledTimes(0);
+        expect(uploadDirectory).toHaveBeenCalledTimes(1);
+      });
+    });
   });
 });
